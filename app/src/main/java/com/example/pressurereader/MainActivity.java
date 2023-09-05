1fb423d50e38643ef89b9a64185bd3617140d795
OrkunKURUN <44517787+OrkunKURUN@users.noreply.github.com>
2023-09-05 14:01:18 +0300

    Main menu added

---
+++ app/src/main/java/com/example/pressurereader/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.pressurereader;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button semi = findViewById(R.id.testSemi);
+        Button full = findViewById(R.id.testBoth);
+
+        semi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(),TestSemiActivity.class);
+                startActivity(i);
+            }
+        });
+
+        full.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+    }
+}
